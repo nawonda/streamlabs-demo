ad394006499e2afa75228cc7f0a79ee1b829e829
jerry.chang <jerry.chang@bitmain.com>
2019-02-22 23:17:16 +0800

    msg

--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <?php
     //A very very simple websocket server    
 
-    $address = '127.0.0.1';
+    $address = 'wss://immense-garden-26748.herokuapp.com';
     $port = '1222';
     
     // Create WebSocket.

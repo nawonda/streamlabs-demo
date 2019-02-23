3c8603f298796266061c7090854c61ca7456604e
jerry.chang <jerry.chang@bitmain.com>
2019-02-23 10:10:42 +0800

    msg

--- webhook.php
+++ webhook.php
@@ -7,17 +7,17 @@ hub.topic=https://api.twitch.tv/helix/users/follows?first=1&to_id=1337& \
 hub.lease_seconds=864000& \
 hub.challenge=HzSGH_h04Cgl6VbDJm7IyXSNSlrhaLvBi9eft3bw
 */
-// if(isset($_GET['hub_challenge']) && !empty($_GET['hub_challenge'])){
-//     $r = $_GET['hub_challenge'];
-//     //Subscription Verify Response
-//     //HzSGH_h04Cgl6VbDJm7IyXSNSlrhaLvBi9eft3bw
-//     echo $r;
-//     file_put_contents('webhook_log.txt', $r, FILE_APPEND);
-// }
+if(isset($_GET['hub_challenge']) && !empty($_GET['hub_challenge'])){
+    $r = $_GET['hub_challenge'];
+    //Subscription Verify Response
+    //HzSGH_h04Cgl6VbDJm7IyXSNSlrhaLvBi9eft3bw
+    echo $r;
+    file_put_contents('webhook_log.txt', $r, FILE_APPEND);
+}
 
-// $data = json_decode( file_get_contents( 'php://input' ), true );
+$body = file_get_contents('php://input');
 
-// file_put_contents('webhook_log.txt', $data, FILE_APPEND);
+file_put_contents('webhook_log.txt', $body, FILE_APPEND);
 
 // if(isset($_POST['data'])){
 //     $r = $_POST['data'];
@@ -27,13 +27,5 @@ hub.challenge=HzSGH_h04Cgl6VbDJm7IyXSNSlrhaLvBi9eft3bw
 //     echo 200;
 // }
 
-
-echo $_GET['hub.challenge'];
-
-file_put_contents('webhook_log.txt', $_GET['hub.challenge'], FILE_APPEND);
-
-$body = file_get_contents('php://input');
-
-file_put_contents('webhook_log.txt', $body, FILE_APPEND);
 
 ?>
